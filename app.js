07ed0faf4528242260928e104c67cbfbf5b0b432
Wardana <149252113+NatsumeAoii@users.noreply.github.com>
2024-05-13 23:09:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,57 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const axios = require('axios');
 
-let totalMessagesSent = 0;
+const customText = 'Bismillah Mati'; // Custom text
 
-async function sendMessages(urls, customText) {
-  try {
-    console.log(`Sending: "${customText}"`);
-    const requests = urls.map((url, index) =>
-      axios
-        .get(`${url}${encodeURIComponent(customText)}`)
-        .then((response) => {
+class MessageSender {
+  constructor() {
+    this.totalMessagesSent = 0;
+  }
+
+  async sendMessages(urls) {
+    try {
+      console.log(`Sending: "${customText}"`);
+      const requests = urls.map(async (url, index) => {
+        try {
+          const response = await axios.get(`${url}${encodeURIComponent(customText)}`);
           if (response.status === 200) {
             return `Link ${index + 1}: OK`;
           } else {
             return `Link ${index + 1}: Unexpected status code: ${response.status}`;
           }
-        })
-        .catch((error) => {
+        } catch (error) {
           return `Link ${index + 1}: API down!: ${error.message}`;
-        })
-    );
-    const results = await Promise.allSettled(requests);
-    results.forEach((result) => {
-      console.log(result.value);
-    });
-    totalMessagesSent++;
-    console.log(`Total messages sent: ${totalMessagesSent}`);
-    console.log('='.repeat(30));    
-  } catch (error) {
-    console.error('Error:', error.message);
+        }
+      });
+      const results = await Promise.allSettled(requests);
+      results.forEach((result) => {
+        console.log(result.value);
+      });
+      this.totalMessagesSent++;
+      console.log(`Total messages sent: ${this.totalMessagesSent}`);
+      console.log('='.repeat(30));
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+
+  async start() {
+    try {
+      const urls = (await fs.readFile('urls.txt', 'utf8')).trim().split('\n');
+      const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+      while (true) {
+        await this.sendMessages(urls);
+        await delay(100); // Adjust delay as needed (0 = means faster)
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
   }
 }
 
 async function main() {
-  try {
-    const urls = fs
-      .readFileSync('urls.txt', 'utf8')
-      .trim()
-      .split('\n');
-    const customText = 'Bismillah Mati'; // Custom text
-    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-    while (true) {
-      await sendMessages(urls, customText);
-    }
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+  const sender = new MessageSender();
+  await sender.start();
 }
 
 main();
